fe5e32c506849ab0fce8c0c3cc84e8d9afca1362
James Golick <james@giraffesoft.ca>
2008-11-11 19:37:10 -0500

    add a really, really basic spec for the queue, just to make sure that it sorta works as a queue

---
+++ spec/queue_collection_spec.rb
@@ -0,0 +1,15 @@
+require File.dirname(__FILE__) + "/spec_helper"
+require "queue_collection"
+
+describe "queue collection" do
+  it "should be able to put and get items" do
+    queue = QueueCollection.new('./')
+    queue.put('key', 'value')
+    queue.take('key').should == 'value'
+  end
+
+  after(:all) do
+    file = File.dirname(__FILE__) + '/../key'
+    File.delete(file) if File.exist?(file)
+  end
+end
