0826dfd8e55e71661c1574c362bb40763b0469d7
James Golick <james@giraffesoft.ca>
2008-11-11 19:58:02 -0500

    make it possible to grab the first item from a queue

--- app.rb
+++ app.rb
@@ -1,3 +1,9 @@
+require 'activesupport'
+
 post '/queue_items' do
   QueueCollection.put(params[:key], params[:value])
+end
+
+post '/queue_items/first' do
+  render QueueCollection.take(params[:key]).to_json
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -7,4 +7,11 @@ describe "queue app" do
       post('/queue_items?key=mykey&value=myvalue')
     end
   end
+
+  describe "on POST to /queue_items/first" do
+    it "should return the first item from the queue" do
+      QueueCollection.stub!(:take).with('mykey').and_return('somethingawesome')
+      post('/queue_items/first?key=mykey').body.should == 'somethingawesome'.to_json
+    end
+  end
 end
