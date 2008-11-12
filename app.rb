7dc328a9fee935ed801434112a07ab7ed61dd699
James Golick <james@giraffesoft.ca>
2008-11-11 20:21:30 -0500

    oops... make the queue actually work

--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
+require 'queue_collection'
 require 'activesupport'
 
+class ::QueueApp
+  class << self
+    def queue
+      @queue ||= QueueCollection.new('./')
+    end
+  end
+end
+
 post '/queue_items' do
-  QueueCollection.put(params[:key], params[:value])
+  QueueApp.queue.put(params[:key], params[:value])
 end
 
 post '/queue_items/first' do
-  render QueueCollection.take(params[:key]).to_json
+  render QueueApp.queue.take(params[:key]).to_json
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -3,14 +3,14 @@ require File.dirname(__FILE__) + "/spec_helper"
 describe "queue app" do
   describe "on POST to /queue_items" do
     it "should push an item on to the queue" do
-      QueueCollection.should_receive(:put).with('mykey', 'myvalue')
+      QueueApp.queue.should_receive(:put).with('mykey', 'myvalue')
       post('/queue_items?key=mykey&value=myvalue')
     end
   end
 
   describe "on POST to /queue_items/first" do
     it "should return the first item from the queue" do
-      QueueCollection.stub!(:take).with('mykey').and_return('somethingawesome')
+      QueueApp.queue.stub!(:take).with('mykey').and_return('somethingawesome')
       post('/queue_items/first?key=mykey').body.should == 'somethingawesome'.to_json
     end
   end
